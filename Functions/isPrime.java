33ab9fd4260adc7949a4541c0d41c8fc240da818
PRACHI PARAB <prachiparab459@gmail.com>
2025-07-10 00:55:53 +0530

    Update isPrime.java

--- Functions/isPrime.java
+++ Functions/isPrime.java
@@ -1,18 +1,19 @@
 public class isPrime {
     public static boolean isPrime1(int n) {
-        boolean isPrime = true;
-        for (int i = 2; i <= n - 1; i++) {
+        if (n == 2) {
+            return true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
-                isPrime = false;
-                break;
+                return false;
             }
         }
-        return isPrime;
+        return true;
     }
 
     public static void main(String[] args) {
         System.out.println(isPrime1(5));
-        System.out.println(isPrime1(6));
 
     }
 }
